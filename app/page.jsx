0f939eec2eacb6a16f2b6fe6c406e903b4c4ec5e
mohammedyh <mohammedyousufhamid@gmail.com>
2024-01-07 01:41:26 +0000

    Fix tailwind class typo and remove default styles

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   const { input, completion, handleInputChange, handleSubmit } =
     useCompletion();
   return (
-    <div className="mx-auto w-full max-w-d py-24 flex flex-col stretch">
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
       <form onSubmit={handleSubmit}>
         <input
           className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-3 shadow-xl p-2 dark:text-black"
